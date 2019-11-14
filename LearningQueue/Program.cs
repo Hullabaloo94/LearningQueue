193f17d81639329395797d7f34a49650c60624b4
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:40:32 +0000

    initial commit

---
+++ LearningQueue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LearningQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Queues are FIFO, they act just like a queue at a supermarket.
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            // Show element at front of queue then remove from the queue.
+            Console.WriteLine(queue.Dequeue());
+            // Show element at front of queue
+            Console.WriteLine(queue.Peek());
+
+
+
+        }
+    }
+}
